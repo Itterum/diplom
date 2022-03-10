d78dc78d0a481d4e8ebd96194467c04668be5c87
s1veme <kpdpav@gmail.com>
2022-03-10 18:08:13 +0300

    changed fields models

--- departments/models.py
+++ departments/models.py
@@ -6,10 +6,10 @@ from utils.model_utils import generate_id
 class Department(models.Model):
     """Кафедры"""
     id = models.CharField(primary_key=True, max_length=10, unique=True, default=generate_id)
-    name = models.CharField('Название кафедры', max_length=150, blank=True, null=True)
-    email = models.CharField('Почта', max_length=50)
-    phone_number = models.CharField('Номер телефона', max_length=50)
-    address = models.CharField('Адрес', max_length=100)
+    name = models.CharField('Название кафедры', max_length=150)
+    email = models.CharField('Почта', max_length=50, blank=True, null=True)
+    phone_number = models.CharField('Номер телефона', max_length=50, blank=True, null=True)
+    address = models.CharField('Адрес', max_length=100, blank=True, null=True)
     manager_department = models.OneToOneField(
         'profiles.Profile',
         related_name='manager_department',
@@ -18,12 +18,12 @@ class Department(models.Model):
         null=True,
         blank=True,
     )
-    description = models.TextField('Описание')
-    basic_timetable_department = models.FileField('Основное рассписание', upload_to='timetable-d/')
-    session_timetable_department = models.FileField('Рассписание сессии', upload_to='timetable-d/')
-    session_absentia_timetable_department = models.FileField('Рассписание сессии заочно', upload_to='timetable-d/')
-    photo = models.ImageField('Фотография', upload_to='department/')
-    photos = models.ImageField('Фотографии', upload_to='department/')
+    description = models.TextField('Описание', blank=True, null=True)
+    basic_timetable_department = models.FileField('Основное рассписание', upload_to='timetable-d/', blank=True, null=True)
+    session_timetable_department = models.FileField('Рассписание сессии', upload_to='timetable-d/', blank=True, null=True)
+    session_absentia_timetable_department = models.FileField('Рассписание сессии заочно', upload_to='timetable-d/', blank=True, null=True)
+    photo = models.ImageField('Фотография', upload_to='department/', blank=True, null=True)
+    photos = models.ImageField('Фотографии', upload_to='department/', blank=True, null=True)
 
     def __str__(self):
         return self.name
--- groups/models.py
+++ groups/models.py
@@ -15,7 +15,7 @@ class Group(models.Model):
     id = models.CharField(primary_key=True, max_length=10, unique=True, default=generate_id)
     code = models.CharField('Код группы', max_length=150)
     email = models.CharField('Почта группы', max_length=50)
-    phone_number = models.CharField('Номер телефона', max_length=50)
+    phone_number = models.CharField('Номер телефона', max_length=50, blank=True, null=True)
     spec = models.ForeignKey('speciality.Speciality', related_name='Speciality', verbose_name='Специальность',
                              on_delete=models.CASCADE, blank=True,
                              null=True)
@@ -25,7 +25,7 @@ class Group(models.Model):
     curator = models.ForeignKey('profiles.Profile', related_name='curator', verbose_name='Куратор',
                                 on_delete=models.CASCADE, blank=True,
                                 null=True)
-    visit_type = models.CharField('Тип группы', max_length=10, choices=VISITTYPE, default=NOT_SPECIFIED)
+    visit_type = models.CharField('Тип группы', max_length=10, choices=VISITTYPE, default=NOT_SPECIFIED, blank=True, null=True)
     start_date = models.DateField('Начало обучения')
     is_session = models.BooleanField('Началась ли сессия?', default=False)
 
