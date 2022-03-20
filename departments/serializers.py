709e7968a25a8de8fcf93feabeff34e9465692cc
s1veme <kpdpav@gmail.com>
2022-03-20 17:06:14 +0300

    added the output of the number of specialties, groups and news

--- departments/serializers.py
+++ departments/serializers.py
@@ -6,7 +6,6 @@ from news.models import News
 from news.serializers import NewsListSerializer
 
 from groups.models import Group
-from speciality.models import Speciality
 
 
 class DepartmentsListSerializer(serializers.ModelSerializer):
@@ -14,15 +13,23 @@ class DepartmentsListSerializer(serializers.ModelSerializer):
 
     news = serializers.SerializerMethodField()
 
-    # TODO:
-    # count_news = serializers.SerializerMethodField()
-    # count_groups = serializers.SerializerMethodField()
-    # count_specialty = serializers.SerializerMethodField()
+    count_news = serializers.IntegerField(source='news_set.count')
+    count_specialty = serializers.IntegerField(
+        source='speciality_set.count'
+    )
+    count_groups = serializers.SerializerMethodField()
 
     class Meta:
         model = Department
         fields = ('id', 'news', 'name', 'email', 'phone_number', 'address',
-                  'description', 'photo', 'photos', 'manager_department')
+                  'description', 'photo', 'photos', 'manager_department',
+                  'count_news', 'count_specialty', 'count_groups')
 
     def get_news(self, obj):
-        return NewsListSerializer(News.objects.filter(department=obj), many=True).data
+        return NewsListSerializer(News.objects.filter(department=obj),
+                                  many=True).data
+
+    def get_count_groups(self, obj):
+        speciality = obj.speciality_set.all()
+        groups = Group.objects.filter(spec__in=speciality).count()
+        return groups
