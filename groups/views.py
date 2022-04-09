97dc8009580931a3e67feaebf5307666c0ceb1da
s1veme <kpdpav@gmail.com>
2022-04-09 13:46:11 +0300

    changed the activation and deactivation of the session for the group and added the binding of users to the group

--- groups/views.py
+++ groups/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
+from profiles.models import Profile
+
 from .models import Group
 from .serializers import GroupsSerializer, GroupDetailSerializer
 
@@ -42,8 +44,15 @@ class GroupsViewSet(viewsets.ModelViewSet):
         groups = Group.objects.filter(id__in=request.data['groups'])
         session = request.data['session']
 
-        for group in groups:
-            group.is_session = session
-            group.save()
+        groups.update(is_session=session)
+
+        return Response({'status': 'ok'}, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['PUT', 'PATCH'], url_path='user-binding')
+    def user_binding(self, request):
+        users = Profile.objects.filter(id__in=request.data['users'])
+        group = request.data['group']
+
+        users.update(group=group)
 
         return Response({'status': 'ok'}, status=status.HTTP_200_OK)
