2c69b7f2aeef23159ea31e2e16596c797a4cdfd2
s1veme <kpdpav@gmail.com>
2022-04-13 14:38:53 +0300

    added new field description in model Timetable

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -8,11 +8,13 @@ class TimetableSerializer(serializers.ModelSerializer):
         model = Timetable
         depth = 1
         fields = ('para', 'visit', 'type', 'id', 'date', 'discipline',
-                  'teacher', 'department', 'group', 'session', 'is_active')
+                  'teacher', 'department', 'group', 'session', 'is_active',
+                  'description')
 
 
 class TimetableCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Timetable
         fields = ('para', 'visit', 'type', 'id', 'date', 'discipline',
-                  'teacher', 'department', 'group', 'session', 'is_active')
+                  'teacher', 'department', 'group', 'session', 'is_active',
+                  'description')
